1a21ef51d0a8d2a3e9e3aca6f728643a5b08d1b8
Petri Kemppainen <petri@genomeinsights.fi>
2026-03-24 15:25:53 +0200

    bugfix

--- R/manhattan.R
+++ R/manhattan.R
@@ -73,7 +73,7 @@ plot_manhattan <- function(map,
   vars <- c(y_vars,col_var,shape_var)
   map_manh[,..vars]
 
-  if(map_manh[OR_id!="ns",length(unique(OR_id))>0]) stop("No outlier regins detected at given significance threshold, aborting")
+  if(map_manh[,length(which(OR_id!="ns"))]==0) stop("No outlier regins detected at given significance threshold, aborting")
 
   layout <- prep_manhattan(
     cbind(map_manh[, .(
